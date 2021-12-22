4d0f759fdafab3b843c694fc88f0d6169613e990
SRY Venkateswara Prasad <61723056+SRYVPRASAD@users.noreply.github.com>
2021-12-22 11:37:04 +1100

    pause

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,19 @@ export class AppComponent {
   title = 'my-app1';
   imageSrc: any;
   i = 0;
-  timer = 3000;
+  pause: boolean = true;
 
-  constructor() {
-  }
 
   imageObject: any = [
     {
       imageUrl: 'assets/images/related-product.jpg',
       title: 'title of image-0',
-      Otherlink: 'https://bimcontent.com/download/towelrail-heated-radiantheating-round-single/',
+      reflink: 'https://bimcontent.com/download/towelrail-heated-radiantheating-round-single/',
     },
     {
       imageUrl: 'assets/images/related-product01.jpg',
       title: 'title of image-1',
-      Otherlink: 'https://bimcontent.com/download/towelrail-heated-radiantheating-round-single/',
+      reflink: 'https://bimcontent.com/download/towelrail-heated-radiantheating-round-single/',
     },
     {
       imageUrl: 'assets/images/related-product02.jpg',
@@ -34,35 +32,26 @@ export class AppComponent {
   ];
 
 
+  constructor() {
+    setInterval(() => {
+      this.ChangeImage(this.imageObject);
+    }, 3000);
+  }
+
+  ngOnInit(): void {
+    this.ChangeImage(this.imageObject);
+  }
+
   ChangeImage = (img) => {
     let count: any;
     (this.i >= img.length) ? this.i = 0 : null;
-    count = this.i++;
+    this.pause ? count = this.i++ : null;
     return this.imageSrc = img[count];
   }
 
-  changeTimerOut = () => {
-    return this.timer = 3000;
+  OpenNewTab(refLink: string) {
+    window.open(refLink, "_blank");
   }
-  changeTimerIn = () => {
-    return this.timer = 1000000;
-  }
-
-  // tslint:disable-next-line: use-lifecycle-interface
-  ngOnInit(): void {
-    this.ChangeImage(this.imageObject);
-
-    setInterval(() => {
-      this.ChangeImage(this.imageObject);
-    }, this.timer);
-
-  }
-
-
-
-
-
-
 
 }
 
